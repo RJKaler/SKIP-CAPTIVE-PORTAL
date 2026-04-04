bbea5cdeaf1de3dde73f00632a1b0ec4a2f9482a
R.J. Kaler <31756802+RJKaler@users.noreply.github.com>
2026-04-04 15:56:37 -0400

    Add proof of concept note to captive-trigger.sh
    Added a note indicating this script is a proof of concept.

--- captive-trigger.sh
+++ captive-trigger.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -e 
 
+#NOTE: This is a proof of concept. So far, all this will do is reintroduce a captive portal if it's lost.... 
+
 { open neverssl.com && echo "succeess"; }  || { echo "error" && exit 1; }
 
