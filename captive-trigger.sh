dc10eeea4d7b16ab2afd21262c988980dbe3cd6a
R.J. Kaler <31756802+RJKaler@users.noreply.github.com>
2026-04-10 14:47:32 -0400

    Change browser command to Google Chrome
    Updated browser command to use Google Chrome instead of 'open' for better reliability.

--- captive-trigger.sh
+++ captive-trigger.sh
@@ -2,5 +2,15 @@
 
 #NOTE: This is a proof of concept. So far, all this will do is reintroduce a captive portal if it's lost.... 
 
-{ open neverssl.com && echo "succeess"; }  || { echo "error" && exit 1; }
 
+#THIS IS OPTIONAL CODE. I use a VPN on public WiFi addresses - at least in some cases so adjust this to your own needs
+#if pgrep surfshark &>/dev/null; then
+#   echo -e "Surfshark processes still active\nCheck killswitch before proceeding"
+#   exit 0
+#else
+#    echo "Surfshark appears to be disconnected. Proceeding"
+#fi
+
+
+#Adjust for your perferred browser. Initially, I used the "open" command. This doesn't always prove reliable. Identify your browser. I use vanilla Chrome on Kubuntu 
+{ google-chrome neverssl.com && echo "succeess"; }  || { echo "error" && exit 1; }
